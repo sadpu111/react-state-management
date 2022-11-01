e1db443d633d82bad43be0e1f3e50d3f5c680629
sadpu111 <dwme7100@gmail.com>
2022-11-02 00:00:10 +0900

    validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, useState } from "react";
-import {useForm} from "react-hook-form"; // 호출 대상을 중괄호로 안묶으면 호출 안됨(not callable)
+import { useForm } from "react-hook-form"; // 호출 대상을 중괄호로 안묶으면 호출 안됨(not callable)
 
 /* function ToDoList() {
   const [todo, setTodo] = useState("");
@@ -28,17 +28,21 @@ import {useForm} from "react-hook-form"; // 호출 대상을 중괄호로 안묶
   } */
 
 function ToDoList() {
-  const {register, watch} = useForm(); // react-hook에서 제공하는 register 함수
-  console.log(watch()) // form의 입력값 추적
+  const { register, watch, handleSubmit, formState } = useForm(); // react-hook에서 제공하는 함수(watch:form의 입력값 추적, handleSubmit: onSubmit 대체)
+  const onValid = (data: any) => {
+    console.log(data);
+  }
+  console.log(formState.errors); // inValid 값 표시. 에러 처리
+  // HTML의 required를 사용하면 외부인이 소스코드를 변경할 여지가 있기 때문에 자바스크립트를 활용하는 것이 안전({required:true}). 이를 사용하면 자동으로 invalid한 input으로 커서 이동. reqired: "메시지 입력" 또는 minLength에 조건과 메시지를 입력하여 invalid일 때 입력한 메시지 출력가능
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email"></input>
-        <input {...register("firstName")} placeholder="First Name"></input>
-        <input {...register("lastName")} placeholder="Last Name"></input>
-        <input {...register("userName")} placeholder="User Name"></input>
-        <input {...register("password")} placeholder="Password"></input>
-        <input {...register("password Confirm")} placeholder="Password Confirm"></input>
+      <form style={{ display: "flex", flexDirection: "column", }} onSubmit={handleSubmit(onValid)}>
+        <input {...register("email", { required: true })} placeholder="Email"></input>
+        <input {...register("firstName", { required: true })} placeholder="First Name"></input>
+        <input {...register("lastName", { required: true })} placeholder="Last Name"></input>
+        <input {...register("userName", { required: true, minLength: 10 })} placeholder="User Name"></input>
+        <input {...register("password", { required: true, minLength: 10 })} placeholder="Password"></input>
+        <input {...register("password Confirm", { required: "Password is required...", minLength: {value: 5, message: "too short"} })} placeholder="Password Confirm"></input>
         <button>add</button>
       </form>
     </div>)
