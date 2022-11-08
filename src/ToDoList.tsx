1ec5c8cbdcb00154c6d368bbf8c842d338114cb8
sadpu111 <dwme7100@gmail.com>
2022-11-08 20:01:44 +0900

    add reg expresion and validation messgages

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,8 +2,8 @@ import React, { ReactElement, useState } from "react";
 import { useForm } from "react-hook-form"; // 호출 대상을 중괄호로 안묶으면 호출 안됨(not callable)
 
 interface IFormData {
-  [key:string]: string;
-  } // index signature 선언
+  [key: string]: string;
+} // index signature 선언
 
 /* function ToDoList() {
   const [todo, setTodo] = useState("");
@@ -32,26 +32,30 @@ interface IFormData {
   } */
 
 function ToDoList() {
-  const { register, watch, handleSubmit, formState: { errors } } = useForm<IFormData>(); // react-hook에서 제공하는 함수(watch:form의 입력값 추적, handleSubmit: onSubmit 대체)
+  const { register, handleSubmit, formState } = useForm<IFormData>(); // react-hook에서 제공하는 함수(watch:form의 입력값 추적, handleSubmit: onSubmit 대체)
   const onValid = (data: any) => {
     console.log(data);
-  }
+  };
+  console.log(formState.errors);
   return (
     <div>
-      <form style={{ display: "flex", flexDirection: "column", }} onSubmit={handleSubmit(onValid)}>
-        <input {...register("email", {
-          required: true, pattern: {
-            value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-            message: "only naver email allowed..."
-          },
-        })} placeholder="Email"></input>
-        <span>
-          {errors.emial?.message}
-        </span>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      ><input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
         <input {...register("firstName", { required: true })} placeholder="First Name"></input>
         <input {...register("lastName", { required: true })} placeholder="Last Name"></input>
-        <input {...register("userName", { required: true, minLength: 10 })} placeholder="User Name"></input>
-        
+        <input {...register("userName", { required: true, minLength: { value: 5, message: "too short" } })} placeholder="User Name"></input>
+        <input {...register("password", { required: true, minLength: { value: 5, message: "too short" } })} placeholder="Password"></input>
         <input {...register("passwordConfirm", { required: "Password is required...", minLength: { value: 5, message: "too short" } })} placeholder="Password Confirm"></input>
         <button>add</button>
       </form>
