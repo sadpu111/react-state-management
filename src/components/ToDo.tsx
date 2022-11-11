078b2a189771592738937cc9bba29863456a2152
sadpu111 <dwme7100@gmail.com>
2022-11-12 00:09:29 +0900

    category butns

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,16 +1,18 @@
-import { useRecoilValue } from "recoil";
+import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "./Atoms";
 
-function ToDo({ text, category }: IToDo) {
-  const onClick = (newCategory: IToDo["category"]) => {
-
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const { currentTarget: { name },
+    } = event;
   }
   return (
     <li>
       <span>{text}</span>
-      {category !== "TO_DO" && <button onClick={() => onClick("TO_DO")}>To Do</button>}
-      {category !== "DOING" && <button onClick={() => onClick("DOING")}>Doing</button>}
-      {category !== "DONE" && <button onClick={() => onClick("DONE")}>Done</button>}
+      {category !== "TO_DO" && (<button name="TO_DO" onClick={onClick}>To Do</button>)}
+      {category !== "DOING" && (<button name="DOING" onClick={onClick}>Doing</button>)}
+      {category !== "DONE" && (<button name="DONE" onClick={onClick}>Done</button>)}
     </li>
   )
 }
