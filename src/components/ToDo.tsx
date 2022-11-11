4c947202d9d3853bc144161a7c5ca0638b3542dd
sadpu111 <dwme7100@gmail.com>
2022-11-12 00:08:56 +0900

    add category buttons

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,13 +1,16 @@
 import { useRecoilValue } from "recoil";
 import { IToDo, toDoState } from "./Atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category }: IToDo) {
+  const onClick = (newCategory: IToDo["category"]) => {
+
+  }
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "TO_DO" && <button onClick={() => onClick("TO_DO")}>To Do</button>}
+      {category !== "DOING" && <button onClick={() => onClick("DOING")}>Doing</button>}
+      {category !== "DONE" && <button onClick={() => onClick("DONE")}>Done</button>}
     </li>
   )
 }
